c575468526d265135b1354c51f593d59329a2bc4
Jordi Baylina <jordi@baylina.cat>
2017-01-05 19:36:31 +0100

    fix browser

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var liquimContracts = require('./js/liquium_rt.js');
 
 module.exports = liquimContracts;
 
-if (window) {
+if (typeof window !== "undefined") {
     window.liquiumContracts = liquimContracts;
 }
